c65996efa3f5626dc977fc2e64bc635cb44cf2c6
Jan Rogalo <96991881+janrogalo@users.noreply.github.com>
2022-06-16 07:47:37 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+   eleventyConfig.addPassthroughCopy('./src/assets/blog');
   eleventyConfig.addPassthroughCopy('./src/admin');
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
